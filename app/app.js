dd08c848ba8aad4a61bc80da84d1ad2c66b1eef9
César Martins <cesar.martins@happycodeschool.com>
2022-11-01 18:55:31 -0300

    added produtos em app.js

---
+++ app/app.js
@@ -0,0 +1,34 @@
+const produtos = [
+    {    
+        id: "prod001",
+        nome : "Fire Stick",
+        preco: 360.05,
+        cor: "preto",
+        imagem: "fire-stick.jpg",
+        descricao: "Fire TV Stick | Streaming em Full HD com Alexa | Com Controle Remoto por Voz com Alexa (inclui comandos de TV)"
+    },
+    {
+        id: "prod002",
+        nome : "Kindle",
+        preco: 299.05,
+        cor: "preto",
+        imagem: "kindle.jpg",
+        descricao: "Kindle 10a. geração com bateria de longa duração - Cor Preta"
+    },
+    {
+        id: "prod003",
+        nome : "Capa Kindle",
+        preco: 69,
+        cor: "preto",
+        imagem: "capa-kindle.jpg",
+        descricao: "Capa de tecido resistente à água para Kindle Paperwhite (10ª Geração não compatível com as versões anteriores do Kindle Paperwhite ou com 11ª Geração lançada em 2021) - Cor Azul"
+    },
+    {
+        id: "prod004",
+        nome : "Echo Dot",
+        preco: 270.55,
+        cor: "preto",
+        imagem: "echo-dot.jpg",
+        descricao: "Echo Dot (3ª Geração): Smart Speaker com Alexa - Cor Preta"       
+    }
+]
